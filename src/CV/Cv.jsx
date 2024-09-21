9e87db84c06e2aef9294e0f0cbe90c6e1447c254
Abdullah Shahed <142104079+shahed32156@users.noreply.github.com>
2024-09-21 13:53:15 +0600

    Add files via upload

--- src/CV/Cv.jsx
+++ src/CV/Cv.jsx
@@ -5,16 +5,16 @@ const Cv = () => {
     <>
     
     <div className='text-center mt-20'>
-          <h1 class="text-3xl text-center text-[#bc2bcc] font-mono font-semibold">My CV</h1>
+          <h1 class="text-3xl text-center text-[#bc2bcc] font-mono font-semibold">My Resume</h1>
     </div>
     
     <div className='mt-10 mx-3 flex flex-col gap-4 justify-center items-center'>
 
         <div>
-        <img className='mx-auto h-[400px] w-[300px]' src="./Images/cv.png" alt="" />
+        <img className='mx-auto h-[400px] w-[300px]' src="./Images/resume.PNG" alt="" />
         </div>
         <div>
-        <a href="./Images/Abdullah Shahed (CV).pdf" download="Abdullah Shahed (CV)" target="_blank"><button class="bg-[#bc2bcc] flex justify-center items-center gap-2 text-white font-[Poppins] duration-500 w-[150px] h-[40px] shadow-lg hover:shadow-[#bc2bcc] rounded"><FaDownload className='text-white text-[16px]' /> Download CV</button></a>
+        <a href="./Images/Abdullah Shahed (Resume).pdf" download="Abdullah Shahed (Resume)" target="_blank"><button class="bg-[#bc2bcc] flex justify-center items-center gap-2 text-white font-[Poppins] duration-500 w-[170px] h-[40px] shadow-lg hover:shadow-[#bc2bcc] rounded"><FaDownload className='text-white text-[16px]' /> Download Resume</button></a>
         </div>
     </div>
 
